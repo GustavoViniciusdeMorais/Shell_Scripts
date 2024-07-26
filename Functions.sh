1567fd9623f88c5768068b507ad9eff2bd1b1323
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2024-07-26 19:11:31 -0300

    - Updated shell function script example

--- Functions.sh
+++ Functions.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-function () {
-    read -p "Type something:" some
-    echo "Your param is $i and you typed $some"
+# Define the function with parameters
+concatenate_strings() {
+    local string1="$1"
+    local string2="$2"
+    echo "${string1}${string2}"
 }
+
+# Define two variables
+a="Hello, "
+b="world!"
+
+# Call the function with variables as arguments
+concatenate_strings "$a" "$b"
