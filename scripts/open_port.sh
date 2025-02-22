69f3c80c95d42fda2c08ae67120eadb826bb0790
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2025-02-22 11:20:55 -0300

    - improved the open ports script, to check for the output of previous command with if 0

--- scripts/open_port.sh
+++ scripts/open_port.sh
@@ -11,21 +11,22 @@ else
     port=$2
 fi
 
-testPing=$(ping -c 1 $host > /dev/null 2>&1)
-netcatTestPort=$(nc -z $host $port > /dev/null 2>&1)
+ping -c 1 $host > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Host $host is reacheble"
+else
+    echo "Host $host is NOT reacheble"
+fi
 
 # nc is netcat
 # -z Only scan for listening daemons
 # > /dev/null Redirects stdout (normal output) to /dev/null, which is a special device that discards anything written to it.
 # 2>&1 Redirects stderr (error output) to stdout.
 
-if $testPing; then
-    echo "Host $host is reacheble"
-else
-    echo "Host $host is NOT reacheble"
-fi
+nc -z $host $port > /dev/null 2>&1
 
-if $netcatTestPort; then
+if [ $? -eq 0 ]; then
     echo "Port $port is open on $host"
 else
     echo "Port $port is closed on $host"
