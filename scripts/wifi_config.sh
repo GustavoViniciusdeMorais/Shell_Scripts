e0a00866bd3d4a40f3cb3ae9d8e2267f3812f9ce
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2025-03-30 11:14:25 -0300

    - wifi interface config script example

---
+++ scripts/wifi_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+interface="wlp0s20f3"
+ssid="Gustavo2G"
+password="Gusbot259#"
+# temp conf file
+wpa_config=$(mktemp)
+cat << EOF > $wpa_config
+ctrl_interface=/var/run/wpa_supplicant
+network={
+    ssid="$ssid"
+    psk="$password"
+}
+EOF
+wpa_supplicant -i $interface -c $wpa_config
+# new ip
+dhclient $interface
+# remove temp conf
+rm $wpa_config
+echo "connected to $ssid"
