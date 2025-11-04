1cc307419531357c9ac4afa6a9314b08d7789fb9
Gustavo <gustavocop14dos@gmail.com>
2025-11-04 10:35:31 -0300

    Create my_banner_cli.sh

---
+++ my_banner_cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ip -c a | head -n 10
+figlet LINUX
+cd /home/gustavo/devsecops/Studies/bashes/ && tree .
+ip -c l
+figlet PHP
+figlet SQL
+for val in {1..2};do echo '{"oses": "Kali Linux"}' | jq . ; done
+systemctl status ollama
